d14216ea5a3e564ed7ff83f9bf1bbf3d3ff94162
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-06 16:00:14 -0500

    Stretch 2: If pokemon doesn't exist (ie. status code is not 200), render 'not found' message with status code 404

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -3,20 +3,25 @@ require 'json'
 class PokemonController < ApplicationController
   def show
     pokemon_response = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{ params[:id] }")
-    pokemon_data = JSON.parse(pokemon_response.body)
-    puts pokemon_data["name"]
 
-    giphy_response = HTTParty.get("http://api.giphy.com/v1/gifs/random?api_key=#{ ENV["GIPHY_KEY"] }&tag=#{ pokemon_data["name"] }&rating=g")
-    giphy_data = JSON.parse(giphy_response.body)
-    puts giphy_data["data"]["image_url"]
+    if pokemon_response.code == 200
+      pokemon_data = JSON.parse(pokemon_response.body)
+      puts pokemon_data["name"]
 
-    render json: {
-      id: pokemon_data["id"],
-      name: pokemon_data["name"],
-      types: pokemon_data["types"].map { |single_type|
-          single_type["type"]["name"]
-        },
-      gif: giphy_data["data"]["image_url"]
-    }
+      giphy_response = HTTParty.get("http://api.giphy.com/v1/gifs/random?api_key=#{ ENV["GIPHY_KEY"] }&tag=#{ pokemon_data["name"] }&rating=g")
+      giphy_data = JSON.parse(giphy_response.body)
+      puts giphy_data["data"]["image_url"]
+
+      render json: {
+        id: pokemon_data["id"],
+        name: pokemon_data["name"],
+        types: pokemon_data["types"].map { |single_type|
+            single_type["type"]["name"]
+          },
+        gif: giphy_data["data"]["image_url"]
+      }
+    else
+      render :plain => 'Not Found', :status => '404'
+    end
   end
 end
