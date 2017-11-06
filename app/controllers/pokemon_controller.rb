82f6f60582aec4de0ec640e69c054e11a57c9871
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-06 15:18:04 -0500

    Render json for pokemon with id, name and types as an array of type names

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -5,6 +5,13 @@ class PokemonController < ApplicationController
     pokemon_response = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{ params[:id] }")
     pokemon_data = JSON.parse(pokemon_response.body)
     puts pokemon_data["name"]
-    render json: { "message": "ok" }
+
+    render json: {
+      id: pokemon_data["id"],
+      name: pokemon_data["name"],
+      types: pokemon_data["types"].map { |single_type|
+        single_type["type"]["name"]
+      }
+    }
   end
 end
