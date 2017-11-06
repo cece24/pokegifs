4042a57e4092edf8160f7bca3fc125de2e7bf751
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-06 15:41:03 -0500

    Add giphy url to json response. Refactor giphy request query string with interpolation of pokemon name, so numeric ids will still return pokemon gifs

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -6,16 +6,17 @@ class PokemonController < ApplicationController
     pokemon_data = JSON.parse(pokemon_response.body)
     puts pokemon_data["name"]
 
-    giphy_response = HTTParty.get("http://api.giphy.com/v1/gifs/search?api_key=#{ ENV["GIPHY_KEY"] }&q=#{ params[:id] }&rating=g&limit=1")
+    giphy_response = HTTParty.get("http://api.giphy.com/v1/gifs/search?api_key=#{ ENV["GIPHY_KEY"] }&q=#{ pokemon_data["name"] }&rating=g&limit=1")
     giphy_data = JSON.parse(giphy_response.body)
-    puts giphy_data
+    puts giphy_data["data"][0]["images"]["fixed_height"]["url"]
 
     render json: {
       id: pokemon_data["id"],
       name: pokemon_data["name"],
       types: pokemon_data["types"].map { |single_type|
-        single_type["type"]["name"]
-      }
+          single_type["type"]["name"]
+        },
+      url: giphy_data["data"][0]["images"]["fixed_height"]["url"]
     }
   end
 end
