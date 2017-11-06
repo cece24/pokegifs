9bb8cd0745b593f160fe7aeea24c5e29380ff13a
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-06 15:10:15 -0500

    Define pokemon#show to make request to Pokemon API with interpolated params[:id]. Parse JSON into Ruby hash

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -2,6 +2,9 @@ require 'json'
 
 class PokemonController < ApplicationController
   def show
+    pokemon_response = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{ params[:id] }")
+    pokemon_data = JSON.parse(pokemon_response.body)
+    puts pokemon_data["name"]
     render json: { "message": "ok" }
   end
 end
